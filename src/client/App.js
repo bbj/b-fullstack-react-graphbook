a5e58036eade1c1f5aff702d9d493062c590f8c8
Bruno Jullien <bruno.jullien@gmail.com>
2022-03-17 20:08:34 -0700

    Chap1 / Event handling and state updates with React

--- src/client/App.js
+++ src/client/App.js
@@ -6,9 +6,36 @@ const App = () => {
   //initiate the state variable 'posts', and its setter function using useState() hook
   //setPosts(somePosts) => state change detected => React re-render App component
   const [posts, setPosts] = useState(initialPosts);
+  const [postContent, setPostContent] = useState('');
+
+  const handleSubmit = (event) => {
+    //stop web browser to actually submit the form and reload the page
+    event.preventDefault();
+    const newPost = {
+      id: posts.length + 1, //used for <div key={post.id} className="post">
+      text: postContent,
+      user: {
+        avatar: '/uploads/avatar1.png',
+        username: 'Fake User'
+      }
+    };
+    //update the "posts" state, merging newPost with current posts
+    //destructured assignment of posts array into new array
+    setPosts([newPost, ...posts]);
+    //clear state: update the "postContent" state
+    setPostContent('');
+  };
 
   return (
     <div className="container">
+      <div className="postForm">
+        <form onSubmit={handleSubmit}>
+          <textarea value={postContent} onChange={(e) =>
+            setPostContent(e.target.value)}
+            placeholder="Write your custom post!" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
       <div className="feed">
         {initialPosts.map((post, i) =>
           <div key={post.id} className="post">
